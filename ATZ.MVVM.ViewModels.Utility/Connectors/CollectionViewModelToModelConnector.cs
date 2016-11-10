adf9c5e86d0aaf78d5471d84485f75ec140d9fde
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2016-11-10 19:07:03 +1100

    Changing TViewModel requirement from BaseViewModel<TModel> to IViewModel<TModel>.

--- ATZ.MVVM.ViewModels.Utility/Connectors/CollectionViewModelToModelConnector.cs
+++ ATZ.MVVM.ViewModels.Utility/Connectors/CollectionViewModelToModelConnector.cs
@@ -11,7 +11,7 @@ namespace ATZ.MVVM.ViewModels.Utility.Connectors
     /// <typeparam name="TModel">The type of the Model.</typeparam>
     // ReSharper disable once ClassWithVirtualMembersNeverInherited.Global => Part of public API.
     public class CollectionViewModelToModelConnector<TViewModel, TModel> : ObservableCollectionConnector<TModel, IViewModel<TModel>>, IVerifiable
-        where TViewModel : BaseViewModel<TModel>, new()
+        where TViewModel : IViewModel<TModel>, new()
         where TModel : class
     {
         /// <summary>
